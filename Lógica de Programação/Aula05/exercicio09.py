7a3b6d39438786815ec5d5588c431554e090d375
Mariana Mendes <filhatronixmmlima@gmail.com>
2024-05-08 14:17:25 -0300

    Aula05e06

--- Lógica de Programação/Aula05/exercicio09.py
+++ Lógica de Programação/Aula05/exercicio09.py
@@ -1,25 +1,30 @@
-nomes = ["mari","lud","joão","izair","lipe"]
-senhas = ["123","456","789","012","345"]
-decisao = int(input("1 - Realizar o login \n "
-                    "2 - Sair: "))
-while decisao == 1:
+nomes = ["","","","",""]
+senhas = ["","","","",""]
+opcao = 0
+cont = 0
 
-    login = input("Digite o nome do seu usuário: ")
+while opcao != 3:
+    opcao = int(input("Escolha sua opção: \n"
+                      "1 - Cadastro\n"
+                      "2 - Login \n"
+                      "3 - Sair"))
 
-    for x in range(5):
-        if login == nomes[x]:
-            senha = input("Digite sua senha: ")
-            for y in range(5):
-                if x == y:
-                    while senha != senhas[y]:
-                        print("Senha errada")
-                        senha = input("Digite sua senha novamente: ")
-                    while senha == senhas[y]:
-                        print("Login efetuado com sucesso")
-                        decisao = 0
-                        break
-            break
-        elif x == 4:
-            print("Usuário não existe")
-            decisao = int(input("1 - Tenta efetuar o login nova \n"
-                                "2 - Sair"))
+    if opcao == 1:
+        for x in range(5):
+            nomes[x] = input("Digite nome do usuário: ")
+            senhas[x] = input("Digite a senha: ")
+
+    elif opcao == 2:
+        login = input("Digite o nome do seu usuário: ")
+        senha = input("Digite sua senha: ")
+        for z in range(5):
+            if login == nomes[z]:
+                        while senha != senhas[z]:
+                            print("Senha errada")
+                            senha = input("Digite sua senha novamente: ")
+                        if senha == senhas[z]:
+                            print("Login efetuado com sucesso")
+            else:
+                cont += 1
+                if cont == 5:
+                    print("Usuário não existe")
