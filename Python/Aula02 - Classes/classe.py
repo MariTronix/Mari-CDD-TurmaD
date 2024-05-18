3a90ad6907a9025abd767e7102fa2ef3759c365a
Mariana Mendes <filhatronixmmlima@gmail.com>
2024-05-17 21:21:16 -0300

    Desafio Pou

--- Python/Aula02 - Classes/classe.py
+++ Python/Aula02 - Classes/classe.py
@@ -10,68 +10,107 @@ class Pessoa():
     def comer(self, alimento):
 
         if self.comendo == False and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} começou a comendo {alimento}")
             self.comendo = True
+            return (f"{self.nome} começou a comer {alimento}")
         elif self.comendo == True and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} já está comendo, não pode comer {alimento} agora.")
+            return (f"{self.nome} está comendo, não pode comer {alimento} agora.")
         elif self.comendo == False and self.falando == True and self.dormindo == False:
-            print(f"{self.nome} está falando, não pode comer {alimento} agora.")
+            return (f"{self.nome} está falando, não pode comer {alimento} agora.")
         elif self.comendo == False and self.falando == False and self.dormindo == True:
-            print(f"{self.nome} está dormindo, não pode comer {alimento} agora.")
+            return (f"{self.nome} está dormindo, não pode comer {alimento} agora.")
 
     def pararDeComer(self):
 
         if self.comendo == True and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} parou de comer")
             self.comendo = False
+            return (f"{self.nome} parou de comer")
         elif self.comendo == False and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} está fazendo nada")
+            return (f"{self.nome} está fazendo nada")
         elif self.comendo == False and self.falando == True and self.dormindo == False:
-            print(f"{self.nome} não está comendo, está falando")
+            return (f"{self.nome} não está comendo, está falando")
         elif self.comendo == False and self.falando == False and self.dormindo == True:
-            print(f"{self.nome} não está comendo, está dormindo")
+            return (f"{self.nome} não está comendo, está dormindo")
 
     def falar(self):
 
         if self.comendo == False and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} começou a falando")
             self.falando = True
+            return (f"{self.nome} começou a falar")
         elif self.comendo == True and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} está comendo, não pode falar")
+            return (f"{self.nome} está comendo, não pode falar")
         elif self.comendo == False and self.falando == True and self.dormindo == False:
-            print(f"{self.nome} já está falando")
+            return (f"{self.nome} está falando")
         elif self.comendo == False and self.falando == False and self.dormindo == True:
-            print(f"{self.nome} não pode falar, está dormindo")
+            return (f"{self.nome} não pode falar, está dormindo")
 
     def pararDeFalar(self):
         if self.comendo == False and self.falando == True and self.dormindo == False:
-            print(f"{self.nome} parou de falar")
             self.falando = False
+            return (f"{self.nome} parou de falar")
         elif self.comendo == True and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} já está comendo")
+            return (f"{self.nome} está comendo")
         elif self.comendo == False and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} está fazendo nada")
+            return (f"{self.nome} está fazendo nada")
         elif self.comendo == False and self.falando == False and self.dormindo == True:
-            print(f"{self.nome} está dormindo")
+            return (f"{self.nome} está dormindo")
 
     def dormir(self):
         if self.comendo == False and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} foi dormir")
             self.dormindo = True
+            return (f"{self.nome} foi dormir")
         elif self.comendo == True and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} já está comendo, não pode ir dormir")
+            return (f"{self.nome} está comendo, não pode ir dormir")
         elif self.comendo == False and self.falando == True and self.dormindo == False:
-            print(f"{self.nome} já está falando, depois que parar de falar. Pode ir dormir")
+            return (f"{self.nome} está falando, não pode ir dormir")
         elif self.comendo == False and self.falando == False and self.dormindo == True:
-            print(f"{self.nome} já está dormindo")
+            return (f"{self.nome} está dormindo")
 
-    def acorda(self):
+    def acordar(self):
         if self.comendo == False and self.falando == False and self.dormindo == True:
-            print(f"{self.nome} Acordou")
             self.dormindo = False
+            return (f"{self.nome} Acordou")
         elif self.comendo == True and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} já está acordado e comendo")
+            return (f"{self.nome} está acordado e comendo")
         elif self.comendo == False and self.falando == True and self.dormindo == False:
-            print(f"{self.nome} já está acordado e falando")
+            return (f"{self.nome} está acordado e falando")
         elif self.comendo == False and self.falando == False and self.dormindo == False:
-            print(f"{self.nome} está fazendo nada")
+            return (f"{self.nome} está fazendo nada")
+
+    def menu(self):
+
+        cont = 0
+
+        while cont != 7:
+            cont = int(input("1 - Comer\n"
+                             "2 - Para de comer\n"
+                             "3 - Falar\n"
+                             "4 - Para de falar\n"
+                             "5 - Dormir\n"
+                             "6 - Acorda\n"
+                             "7 - Sair\n "))
+
+            while cont < 0 or cont > 7:
+                cont = int(input("Escolha uma dessas opções:\n"
+                                 "1 - Comer\n"
+                                 "2 - Para de comer\n"
+                                 "3 - Falar\n"
+                                 "4 - Para de falar\n"
+                                 "5 - Dormir\n"
+                                 "6 - Acorda\n"
+                                 "7 - Sair\n "))
+
+            if cont == 1:
+                alimento = input("O que deseja comer? ")
+                print(self.comer(alimento))
+            elif cont == 2:
+                print(self.pararDeComer())
+            elif cont == 3:
+                print(self.falar())
+            elif cont == 4:
+                print(self.pararDeFalar())
+            elif cont == 5:
+                print(self.dormir())
+            elif cont == 6:
+                print(self.acordar())
+            elif cont == 7:
+                print("Saindo...")
--- Python/Aula02 - Classes/main.py
+++ Python/Aula02 - Classes/main.py
@@ -1,46 +1,5 @@
 from classe import *
 
 p1 = Pessoa("Mari", 100, 27)
-p2 = Pessoa("Maria", 86, 53)
-p3 = Pessoa("Mario", 90, 54)
-p4 = Pessoa("Mayara", 60, 31)
 
-p1.comer("Pizza")
-p1.comer("Uva")
-p1.falar()
-p1.dormir()
-p1.acorda()
-p1.pararDeFalar()
-
-print()
-
-p1.pararDeComer()
-p1.falar()
-p1.falar()
-p1.comer("Pizza")
-p1.pararDeComer()
-p1.dormir()
-p1.acorda()
-p1.pararDeFalar()
-
-print()
-
-p1.pararDeFalar()
-p1.dormir()
-p1.dormir()
-p1.falar()
-p1.pararDeFalar()
-p1.comer("Pizza")
-p1.pararDeComer()
-p1.acorda()
-
-print()
-
-p1.acorda()
-
-
-
-
-
-
-
+p1.menu()
