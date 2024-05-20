b15b50d9dc62955a4733ca124ce54566a80c2926
Mariana Mendes <filhatronixmmlima@gmail.com>
2024-05-20 20:37:16 -0300

    desafio

---
+++ Python/Desafio/desafio2.py
@@ -0,0 +1,16 @@
+class PedraPaPelTesoura:
+    def __init__(self, jogador1, jogador2):
+        self.jogador1 = jogador1
+        self.jogador2 = jogador2
+
+    def primeiraJogada(self, primeiraJogada):
+        primeiraJogada = input(f"{self.jogador1} Escolha: \n"
+                                "Pedra \n"
+                                "Papel \n"
+                                "Tesoura \n")
+    def segundaJogada(self,SegundaJogada):
+        segundaJogada = input(f"{self.jogador2} Escolha: \n"
+                                "Pedra \n"
+                                "Papel \n"
+                                "Tesoura \n")
+
---
+++ Python/Desafio/main.py
@@ -0,0 +1,4 @@
+from desafio2 import *
+
+p1 = PedraPaPelTesoura("Lipinho", "Vitorinha")
+
