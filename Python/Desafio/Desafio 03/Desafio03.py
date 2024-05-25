5247f78849efa2c45e10b6a129f9b22a1c926a24
Mariana Mendes <filhatronixmmlima@gmail.com>
2024-05-25 09:36:10 -0300

    Desafio 03 - Jogo da Velha

--- Python/Desafio/Desafio 03/Desafio03.py
+++ Python/Desafio/Desafio 03/Desafio03.py
@@ -5,118 +5,132 @@ class JogoDaVelha():
         self.escolhaDoJogador1 = "X"
         self.escolhaDoJogador2 = "O"
 
-    """def escolha(self):
-        self.escolhaDoJogador1 = input(f"{self.jogador1}, você escolhe X  ou O: ").upper()
-        while self.escolhaDoJogador1 not in ["X", "O", "0"]:
-            self.escolhaDoJogador1 = input(f"{self.jogador1}, você escolhe X  ou O: ").upper()
-
-        if self.escolhaDoJogador1 == "X":
-            self.escolhaDoJogador2 = "O"
-        else:
-            self.escolhaDoJogador2 = "X"
-
-        print(self.escolhaDoJogador1)
-        print(self.escolhaDoJogador2)"""
-
     def resultado(self):
-
         matriz = [
-            ["-","-","-"],
-            ["-","-","-"],
-            ["-","-","-"]
+            ["1", "2", "3"],
+            ["4", "5", "6"],
+            ["7", "8", "9"]
         ]
 
+        listaDeJogada = []
+
+        print(f"{self.jogador1} = X \n{self.jogador2} = O")
         for x in range(0, 3):
             for y in range(0, 3):
                 print(matriz[x][y], end="")
             print()
 
-        print("Linhas A,B e C e Colunas 1,2 e 3 \n Jogador 1 = X \n Jogador 2 = O")
+            for rodada in range(9):
 
-        primeiraJogadaDo01 = input(f"Qual é a sua primeira jogada {self.jogador1}: ").upper()
-        if primeiraJogadaDo01 in ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]:
-            if primeiraJogadaDo01 == "A1":
-                matriz[0].insert(0,"X")
-            elif primeiraJogadaDo01 == "A2":
-                matriz[0].insert(1,"X")
-            elif primeiraJogadaDo01 == "A3":
-                matriz[0].insert(2,"X")
-            elif primeiraJogadaDo01 == "B1":
-                matriz[1].insert(0, "X")
-            elif primeiraJogadaDo01 == "B2":
-                matriz[1].insert(1, "X")
-            elif primeiraJogadaDo01 == "B3":
-                matriz[1].insert(2, "X")
-            elif primeiraJogadaDo01 == "C1":
-                matriz[2].insert(0, "X")
-            elif primeiraJogadaDo01 == "C2":
-                matriz[2].insert(1, "X")
-            elif primeiraJogadaDo01 == "C3":
-                matriz[2].insert(2, "X")
+                # Jogador 1
+                jogadaDoJogador1 = input(f"Escolha entre 1 e 9 \nQual é a sua jogada {self.jogador1}: ")
+                while jogadaDoJogador1 in listaDeJogada:
+                    jogadaDoJogador1 = input(
+                        f"Essa jogada já foi escolhida. \nQual é a sua jogada {self.jogador1}: ")
+                while jogadaDoJogador1 not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                    jogadaDoJogador1 = input(
+                        f"Jogada inválida, tente novamente. \nQual é a sua jogada {self.jogador1}: ")
 
-        for x in range(0, 3):
-            for y in range(0, 3):
-                print(matriz[x][y], end=" ")
-            print()
+                listaDeJogada.append(jogadaDoJogador1)
+                print(listaDeJogada)
 
-        primeiraJogadaDo02 = input(f"Qual é a sua primeira jogada {self.jogador2}")
+                # Verificar se o jogador 1 venceu
+                if rodada >= 2:
+                    if (matriz[0][0] == "X" and matriz[0][1] == "X" and matriz[0][2] == "X") \
+                            or (matriz[1][0] == "X" and matriz[1][1] == "X" and matriz[1][2] == "X") \
+                            or (matriz[2][0] == "X" and matriz[2][1] == "X" and matriz[2][2] == "X") \
+                            or (matriz[0][0] == "X" and matriz[1][0] == "X" and matriz[2][0] == "X") \
+                            or (matriz[0][1] == "X" and matriz[1][1] == "X" and matriz[2][1] == "X") \
+                            or (matriz[0][2] == "X" and matriz[1][2] == "X" and matriz[2][2] == "X") \
+                            or (matriz[0][0] == "X" and matriz[1][1] == "X" and matriz[2][2] == "X") \
+                            or (matriz[0][2] == "X" and matriz[1][1] == "X" and matriz[2][0] == "X"):
+                        print(f"{self.jogador1} Venceu")
+                        print()
+                        break
+                elif rodada == 9:
+                    print("Empate")
 
-        if primeiraJogadaDo01 in ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]:
-            if primeiraJogadaDo02 == "A1":
-                matriz[0].insert(0,"X")
-            elif primeiraJogadaDo01 == "A2":
-                matriz[0].insert(1,"X")
-            elif primeiraJogadaDo02 == "A3":
-                matriz[0].insert(2,"X")
-            elif primeiraJogadaDo02 == "B1":
-                matriz[1].insert(0, "X")
-            elif primeiraJogadaDo02 == "B2":
-                matriz[1].insert(1, "X")
-            elif primeiraJogadaDo02 == "B3":
-                matriz[1].insert(2, "X")
-            elif primeiraJogadaDo02 == "C1":
-                matriz[2].insert(0, "X")
-            elif primeiraJogadaDo02 == "C2":
-                matriz[2].insert(1, "X")
-            elif primeiraJogadaDo02 == "C3":
-                matriz[2].insert(2, "X")
+                #Posição da jogada do jogador 01
 
-        for x in range(0, 3):
-            for y in range(0, 3):
-                print(matriz[x][y], end=" ")
-            print()
+                if jogadaDoJogador1 == "1":
+                    matriz[0][0] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "2":
+                    matriz[0][1] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "3":
+                    matriz[0][2] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "4":
+                    matriz[1][0] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "5":
+                    matriz[1][1] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "6":
+                    matriz[1][2] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "7":
+                    matriz[2][0] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "8":
+                    matriz[2][1] = self.escolhaDoJogador1
+                elif jogadaDoJogador1 == "9":
+                    matriz[2][2] = self.escolhaDoJogador1
 
-        segundaJogadaDo01 = input(f"Qual é a sua primeira jogada {self.jogador1}")
-        if segundaJogadaDo01 == "A1":
-            matriz[0].insert(0, "X")
-        elif segundaJogadaDo01 == "A2":
-            matriz[0].insert(1, "X")
-        elif segundaJogadaDo01 == "A3":
-            matriz[0].insert(2, "X")
-        elif segundaJogadaDo01 == "B1":
-            matriz[1].insert(0, "X")
-        elif segundaJogadaDo01 == "B2":
-            matriz[1].insert(1, "X")
-        elif segundaJogadaDo01 == "B3":
-            matriz[1].insert(2, "X")
-        elif segundaJogadaDo01 == "C1":
-            matriz[2].insert(0, "X")
-        elif segundaJogadaDo01 == "C2":
-            matriz[2].insert(1, "X")
-        elif segundaJogadaDo01 == "C3":
-            matriz[2].insert(2, "X")
+                for x in range(0, 3):
+                    for y in range(0, 3):
+                        print(matriz[x][y], end="")
+                    print()
 
-        for x in range(0, 3):
-            for y in range(0, 3):
-                print(matriz[x][y], end=" ")
-            print()
+                # Jogador 2
 
-    segundaJogadaDo02 = input(f"Qual é a sua primeira jogada {self.jogador2}")
+                jogadaDoJogador2 = input(f"Qual é a sua jogada {self.jogador2}: ")
 
+                while jogadaDoJogador2 in listaDeJogada:
+                    jogadaDoJogador2 = input(f"Essa jogada já foi escolhida. \nQual é a sua jogada {self.jogador2}: ")
+                while jogadaDoJogador2 not in ["1", "2", "3", "4", "5", "6", "7", "8", "9"]:
+                    jogadaDoJogador2 = input(f"Jogada inválida, tente novamente. \nQual é a sua jogada {self.jogador2}: ")
 
+                listaDeJogada.append(jogadaDoJogador2)
+                print(listaDeJogada)
 
+                # Verificar se o jogador 2 venceu
 
+                if rodada >= 2:
+                    if (matriz[0][0] == "O" and matriz[0][1] == "O" and matriz[0][2] == "O") \
+                            or (matriz[1][0] == "O" and matriz[1][1] == "O" and matriz[1][2] == "O") \
+                            or (matriz[2][0] == "O" and matriz[2][1] == "O" and matriz[2][2] == "O") \
+                            or (matriz[0][0] == "O" and matriz[1][0] == "O" and matriz[2][0] == "O") \
+                            or (matriz[0][1] == "O" and matriz[1][1] == "O" and matriz[2][1] == "O") \
+                            or (matriz[0][2] == "O" and matriz[1][2] == "O" and matriz[2][2] == "O") \
+                            or (matriz[0][0] == "O" and matriz[1][1] == "O" and matriz[2][2] == "O") \
+                            or (matriz[0][2] == "O" and matriz[1][1] == "O" and matriz[2][0] == "O"):
+                        print(f"{self.jogador2} Venceu")
+                        print()
+                    elif rodada == 9:
+                        print("Empate")
 
+                # Posição da jogada do jogador 02
+
+                if jogadaDoJogador2 == "1":
+                    matriz[0][0] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "2":
+                    matriz[0][1] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "3":
+                    matriz[0][2] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "4":
+                    matriz[1][0] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "5":
+                    matriz[1][1] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "6":
+                    matriz[1][2] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "7":
+                    matriz[2][0] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "8":
+                    matriz[2][1] = self.escolhaDoJogador2
+                elif jogadaDoJogador2 == "9":
+                    matriz[2][2] = self.escolhaDoJogador2
+
+                for x in range(0, 3):
+                    for y in range(0, 3):
+                        print(matriz[x][y], end="")
+                    print()
+
+        print("Jogo Finalizado")
 
 p1 = JogoDaVelha("Mari", "Lipe")
 p1.resultado()
