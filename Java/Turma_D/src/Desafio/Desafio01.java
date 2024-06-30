64c5200b984b1920c9f591c1150309f2643808ce
Mariana Mendes <filhatronixmmlima@gmail.com>
2024-06-30 15:49:05 -0300

    Deasafio 01 de Java feito

---
+++ Java/Turma_D/src/Desafio/Desafio01.java
@@ -0,0 +1,16 @@
+package Desafio;
+
+import java.util.Scanner;
+
+public class Desafio01 {
+
+	public static void main(String[] args) {
+		char esc;
+		
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Selecione: [M] - Masculino [F]-Feminino");
+		esc = entrada.next().charAt(0);
+
+	}
+
+}
