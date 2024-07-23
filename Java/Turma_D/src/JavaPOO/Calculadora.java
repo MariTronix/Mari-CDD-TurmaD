1ca31e2187b71d669d9ff52604fd032d6e5b999f
Mariana Mendes <filhatronixmmlima@gmail.com>
2024-07-23 16:27:32 -0300

    poli

--- Java/Turma_D/src/JavaPOO/Calculadora.java
+++ Java/Turma_D/src/JavaPOO/Calculadora.java
@@ -4,14 +4,14 @@ public class Calculadora {
 
 	public static void main(String[] args) {
 		CalcularMetodos num = new CalcularMetodos();
-		System.out.println(num.somar2num(10, 10));
-		System.out.println(num.somar3num(1, 20, 30));
-		System.out.println(num.sub2num(10, 10));
-		System.out.println(num.sub3num(1, 20, 30));
-		System.out.println(num.mult2num(10, 10));
-		System.out.println(num.mult3num(1, 20, 30));
-		System.out.println(num.div2num(20, 0));
-		System.out.println(num.div3num(2, 2, 100));
+		System.out.println(num.somar(10, 10));
+		System.out.println(num.somar(1, 20, 30));
+		System.out.println(num.sub(10, 10));
+		System.out.println(num.sub(1, 20, 30));
+		System.out.println(num.mult(10, 10));
+		System.out.println(num.mult(1, 20, 30));
+		System.out.println(num.div(20, 0));
+		System.out.println(num.div(2, 2, 100));
 	}
 
 }
---
+++ Java/Turma_D/src/Polimorfismo/formas.java
@@ -0,0 +1,13 @@
+package Polimorfismo;
+
+public abstract class formas {
+	
+	public void CalcArea() {
+		System.out.println();
+	}
+	
+	public void CalcPerimetro() {
+		System.out.println();
+	}
+
+}
