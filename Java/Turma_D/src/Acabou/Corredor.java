4510c7ca287a0fdaab7452ce581e4e977d1bcb52
Mariana Mendes <filhatronixmmlima@gmail.com>
2024-07-25 15:46:34 -0300

    Ultimo dia

---
+++ Java/Turma_D/src/Acabou/Corredor.java
@@ -0,0 +1,7 @@
+package Acabou;
+
+public interface Corredor extends Atleta {
+	public boolean descansa;
+	
+	public void Correr();
+}
---
+++ Java/Turma_D/src/Acabou/olimpiadas.java
@@ -0,0 +1,9 @@
+package Acabou;
+
+public class olimpiadas extends TriAtleta {
+	public static void main(String[] args) {
+		TriAtleta p1 = new TriAtleta();
+		p1.Correr();
+		p1.pararCorrer();
+	}
+}
